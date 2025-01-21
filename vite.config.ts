3e02ca6d6fec9442f1b118000117ad287a4e4e08
Karthik-reddie <129923946+Karthik-reddie@users.noreply.github.com>
2025-01-21 17:49:29 +0530

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+  server: {
+    headers: {
+      "Cross-Origin-Embedder-Policy": "require-corp",
+      "Cross-Origin-Opener-Policy": "same-origin"
+    }
+  }
+});
